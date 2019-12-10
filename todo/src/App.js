f5dd0dc808459038e28637ddc6aef00b5881a8d9
lukasjaronis <jaronisluke13@gmail.com>
2019-12-10 18:43:35 -0500

    done

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useReducer} from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
+import {initialState, todoReducer} from './reducers/reducer.js'
+
+import TODOform from './components/TODOform'
+import TODOlist from './components/TODOlist'
+
+const AppStles = styled.div`
+
+display: flex;
+justify-content: center;
+align-items: flex-start;
+background: #314455;
+height: 100vh;
+
+
+h1 {
+ color: #fff;
+}
+`;
+
+
 function App() {
-  return (
+  
+  const [ state, dispatch ] = useReducer(todoReducer, initialState );
+  console.log("TCL: App -> dispatch", dispatch)
+  console.log("TCL: App -> state", state)
+
+  const addTodo = item => {
+    dispatch({type: "ADD_TODO", payload: item})
+  }
+
+  const toggleTodo = id => {
+    dispatch({type: "TOGGLE_TODO", payload: id });
+  };
+
+  const clearTodo = id => {
+    dispatch({type: "CLEAR_COMPLETED"});
+  };
+
+return (
+  <AppStles>
     <div className="App">
- 
+    <h1>A TODO App</h1>
+    <TODOlist todoArr={state.todoArr} toggleTodo={toggleTodo} />
+    <TODOform addTodo={addTodo} clearTodo={clearTodo}/>
     </div>
+    </AppStles>
   );
 }
 
---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,50 @@
+export const initialState={
+    todoArr:[
+        {
+            item: 'Learning about reducers',
+            completed: false,
+            id: 1
+        }
+    ]
+    
+};
+
+
+
+export const todoReducer = (state, action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+        const newTodo ={
+            item: action.payload,
+            completed:false,
+            id: Date.now()
+        };
+        return{
+            ...state, todoArr: [...state.todoArr, newTodo]
+        };
+
+    case 'TOGGLE_TODO':
+        return{
+            ...state, todoArr: state.todoArr.map(todo=>{
+                if (todo.id===action.payload){
+                    return{
+                        ...todo, 
+                        completed: !todo.completed
+                    }
+                } else {
+                    return todo;
+                }
+            })
+        };
+    
+    case 'CLEAR_COMPLETED':
+        return{
+            ...state, 
+            todoArr:state.todoArr.filter(todo => !todo.completed)
+        }
+
+    
+    default:
+      return state
+  }
+};
